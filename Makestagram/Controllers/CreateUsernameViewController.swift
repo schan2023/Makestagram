23490ebc5424504d2d5dd4b872775299e0a9efbc
schan2023 <lamschan996@ufl.edu>
2018-07-11 16:29:07 -0700

    Add usernames to database

--- Makestagram/Controllers/CreateUsernameViewController.swift
+++ Makestagram/Controllers/CreateUsernameViewController.swift
@@ -8,6 +8,8 @@
 
 import Foundation
 import UIKit
+import FirebaseAuth
+import FirebaseDatabase
 
 class CreateUsernameViewController: UIViewController {
     
@@ -25,6 +27,28 @@ class CreateUsernameViewController: UIViewController {
     @IBAction func nextButtonTapped(_ sender: Any) {
         //create new user and store in database
         
+        //1) we guard to check that a FIRUser is logged in and a username is provided in text field
+        guard let firUser = Auth.auth().currentUser,
+            let username = usernameTextField.text,
+            !username.isEmpty else { return }
+        
+        //2) create dictionary to store username
+        let userAttrs = ["username": username]
+        
+        //3) specify relative path for where we want to store our data
+        let ref = Database.database().reference().child("users").child(firUser.uid)
+        
+        //4) Write data we want to store
+        ref.setValue(userAttrs) { (error, ref) in
+            if let error = error {
+                assertionFailure(error.localizedDescription)
+                return
+            }
+            //Read user we just wrote to database and create new user from snapshot
+            ref.observeSingleEvent(of: .value, with: {(snapshot) in
+                let user = User(snapshot: snapshot)
+            })
+        }
     }
     
 }
